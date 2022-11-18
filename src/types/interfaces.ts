e3cec447b6e5e5423226d97054aef576a421948d
Damian Kubecki <damian_094@wp.pl>
2022-11-18 15:47:08 +0100

    Update types

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -1,7 +1,53 @@
+import { TicketTypes } from './types';
+
 export interface IRouterPaths {
   home: string;
   admin: string;
   buyTicket: string;
   reservation: string;
   summary: string;
+}
+
+export interface IMovie {
+  id: string;
+  title: string;
+  dates: Date[];
+  description: string;
+  fullDescription?: string;
+  duration?: string;
+  minAge?: number;
+  imageURL?: string;
+  genres?: string[];
+  rate?: number;
+}
+
+export interface ITicket {
+  id: string;
+  owner: `${IUser['firstname']} ${IUser['lastname']}`;
+  movie: IMovie['title'];
+  date: Date;
+  price: string;
+  row: number;
+  seat: number;
+  type: TicketTypes;
+  codeQR: string;
+}
+
+export interface IUser {
+  firstname: string;
+  lastname: string;
+  email: string;
+  phone: number;
+  tickets: ITicket['id'][];
+  wantToWatchIds: IMovie['id'];
+}
+
+export interface IOrder {
+  firstname: IUser['firstname'];
+  lastname: IUser['lastname'];
+  email: IUser['email'];
+  sendPromo: boolean;
+  totalPrice: number;
+  phone?: IUser['phone'];
+  discountCode?: string;
 }
---
+++ src/types/types.ts
@@ -0,0 +1,1 @@
+export type TicketTypes = 'normalny' | 'ulgowy' | 'rodzinny' | 'voucher';
