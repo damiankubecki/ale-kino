f02bf85c2b84f677ab1ab94e648b1d57d864c740
Damian Kubecki <damian_094@wp.pl>
2022-11-18 09:03:42 +0100

    Add views file

---
+++ src/app/views/views.ts
@@ -0,0 +1,13 @@
+import { AdminViewComponent } from './admin-view/admin-view.component';
+import { HomeViewComponent } from './home-view/home-view.component';
+import { BuyTicketViewComponent } from './buy-ticket-view/buy-ticket-view.component';
+import { ReservationViewComponent } from './reservation-view/reservation-view.component';
+import { SummaryViewComponent } from './summary-view/summary-view.component';
+
+export {
+  AdminViewComponent,
+  HomeViewComponent,
+  BuyTicketViewComponent,
+  ReservationViewComponent,
+  SummaryViewComponent,
+};
