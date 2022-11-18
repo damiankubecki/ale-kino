2291bb29bf5737015c32c00a01863d88778b9e70
Damian Kubecki <damian_094@wp.pl>
2022-11-18 09:32:38 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from '../router/app-routing.module';
+import { HomeViewComponent } from './views/home-view/home-view.component';
+import { AdminViewComponent } from './views/admin-view/admin-view.component';
+import { ReservationViewComponent } from './views/reservation-view/reservation-view.component';
+import { BuyTicketViewComponent } from './views/buy-ticket-view/buy-ticket-view.component';
+import { SummaryViewComponent } from './views/summary-view/summary-view.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeViewComponent,
+    AdminViewComponent,
+    ReservationViewComponent,
+    BuyTicketViewComponent,
+    SummaryViewComponent,
+    HeaderComponent,
+    FooterComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
