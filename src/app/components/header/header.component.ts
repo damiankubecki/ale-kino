79bd4e538d4cb2ab7e1b256039de63c483e22576
Damian Kubecki <damian_094@wp.pl>
2022-11-18 15:22:06 +0100

    Update header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  faShoppingCart = faShoppingCart;
+  isLoggedIn = true;
+  name = 'Damian';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
+
 
 }
