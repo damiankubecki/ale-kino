16e25aeaa47317c2982f9bdefe0bbe31fc7ff729
Damian Kubecki <damian_094@wp.pl>
2022-11-23 13:41:19 +0100

    Update repertoire service

--- src/app/services/repertoire/repertoire.service.ts
+++ src/app/services/repertoire/repertoire.service.ts
@@ -1,15 +1,26 @@
 import { Injectable } from '@angular/core';
+import * as moment from 'moment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RepertoireService {
-  NUMBER_OF_DAYS_TO_DISPLAY = 7;
-  DAY_TO_DISPLAY = 'today';
+  // on init displays today
+  private _DAY_TO_DISPLAY = moment().format('DD/MM/YYYY');
+  // on init displays list of 7 days
+  private _NUMBER_OF_DAYS_TO_DISPLAY = 7;
 
   constructor() {}
 
-  setDayToDisplay(day: string) {
-    this.DAY_TO_DISPLAY = day;
+  public get DAY_TO_DISPLAY() {
+    return this._DAY_TO_DISPLAY;
+  }
+
+  public set DAY_TO_DISPLAY(day: string) {
+    if (day) this._DAY_TO_DISPLAY = day;
+  }
+
+  public get NUMBER_OF_DAYS_TO_DISPLAY() {
+    return this._NUMBER_OF_DAYS_TO_DISPLAY;
   }
 }
