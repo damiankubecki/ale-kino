2a1c59be777cb54249a7113f9505f31aa15c3469
Damian Kubecki <damian_094@wp.pl>
2022-11-29 16:36:07 +0100

    Add app config file

---
+++ src/config.ts
@@ -0,0 +1,18 @@
+import { LongDate } from '@myTypes/types';
+import * as moment from 'moment';
+
+interface IRepertoireConfig {
+  DAY_TO_DISPLAY_ON_INIT: LongDate;
+  NUMBER_OF_DAYS_TO_DISPLAY: number;
+}
+
+export interface IConfig {
+  repertoire: IRepertoireConfig;
+}
+
+export const config: IConfig = {
+  repertoire: {
+    DAY_TO_DISPLAY_ON_INIT: moment().format('DD/MM/YYYY') as LongDate,
+    NUMBER_OF_DAYS_TO_DISPLAY: 7,
+  },
+};
