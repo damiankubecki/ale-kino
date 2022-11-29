f2ce3a9f34c8b67d1b5b950e03ac7da3f7811b59
Damian Kubecki <damian_094@wp.pl>
2022-11-29 16:35:21 +0100

    Update repertoire service

--- src/app/services/repertoire/repertoire.service.ts
+++ src/app/services/repertoire/repertoire.service.ts
@@ -1,25 +1,64 @@
 import { Injectable } from '@angular/core';
+import { IMovie } from '@myTypes/interfaces';
 import { LongDate } from '@myTypes/types';
-import * as moment from 'moment';
+import { moviesCollection, moviesDates } from 'data/movies';
+import { BehaviorSubject } from 'rxjs';
+import { config } from 'config';
+
+export interface IMovieWithHours extends IMovie {
+  hours: string[];
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class RepertoireService {
-  private _DAY_TO_DISPLAY: LongDate = moment().format('DD/MM/YYYY') as LongDate; // today
-  private _NUMBER_OF_DAYS_TO_DISPLAY = 7;
+  private _DAY_TO_DISPLAY = new BehaviorSubject<LongDate>(config.repertoire.DAY_TO_DISPLAY_ON_INIT);
+  private _NUMBER_OF_DAYS_TO_DISPLAY = config.repertoire.NUMBER_OF_DAYS_TO_DISPLAY;
+  private _MOVIES_TO_DISPLAY = new BehaviorSubject<IMovieWithHours[]>([]);
 
-  constructor() {}
+  constructor() {
+    this.setDayToDisplay(config.repertoire.DAY_TO_DISPLAY_ON_INIT);
+  }
+
+  public setDayToDisplay(date: LongDate) {
+    this._DAY_TO_DISPLAY.next(date);
+
+    this._MOVIES_TO_DISPLAY.next(
+      moviesCollection
+        .filter(movie => {
+          const movieDates = moviesDates.find(m => m.id === movie.id);
+
+          const isMoviePlayToday = movieDates?.dates.filter(
+            date => date.day === this._DAY_TO_DISPLAY.value
+          ).length;
+
+          return isMoviePlayToday;
+        })
+        .map(movie => {
+          const movieDates = moviesDates.find(m => m.id === movie.id);
+
+          const hours = movieDates?.dates
+            .filter(date => date.day === this.DAY_TO_DISPLAY.value)
+            .map(date => date.hours)
+            .flat();
+
+          const movieWithHours = { ...movie, hours: hours } as IMovieWithHours;
+
+          return movieWithHours;
+        })
+    );
+  }
 
   public get DAY_TO_DISPLAY() {
     return this._DAY_TO_DISPLAY;
   }
 
-  public setDayToDisplay(date: LongDate) {
-    this._DAY_TO_DISPLAY = date;
-  }
-
   public get NUMBER_OF_DAYS_TO_DISPLAY() {
     return this._NUMBER_OF_DAYS_TO_DISPLAY;
+  }
+
+  public get MOVIES_TO_DISPLAY() {
+    return this._MOVIES_TO_DISPLAY;
   }
 }
