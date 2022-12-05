4ecea28a06f5d0a606b10e2ea27de8db0468d0bf
Damian Kubecki <damian.kubecki@ppuc.pl>
2022-12-05 16:11:19 +0100

    Update home-view component

--- src/app/views/home-view/home-view.component.ts
+++ src/app/views/home-view/home-view.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { IUser } from '@myTypes/interfaces';
+import { UserService } from '@app/services/user/user.service';
+import paths from 'router/paths';
 
 @Component({
   selector: 'app-home-view',
   templateUrl: './home-view.component.html',
-  styleUrls: ['./home-view.component.scss']
+  styleUrls: ['./home-view.component.scss'],
 })
-export class HomeViewComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class HomeViewComponent {
+  constructor(private userService: UserService, private router: Router) {
+    this.userService.USER_DATA.subscribe(user => {
+      if (user?.type === 'admin') {
+        this.router.navigate([paths.admin]);
+      }
+    });
   }
-
 }
