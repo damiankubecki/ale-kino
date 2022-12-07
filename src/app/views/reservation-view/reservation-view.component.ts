1ad78dab3469f11ad9eb9010f7daea04610ff70a
Damian Kubecki <damian.kubecki@ppuc.pl>
2022-12-07 16:46:41 +0100

    Update reservation-view component

--- src/app/views/reservation-view/reservation-view.component.ts
+++ src/app/views/reservation-view/reservation-view.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-reservation-view',
   templateUrl: './reservation-view.component.html',
-  styleUrls: ['./reservation-view.component.scss']
+  styleUrls: ['./reservation-view.component.scss'],
 })
-export class ReservationViewComponent implements OnInit {
+export class ReservationViewComponent {
+  title = 'Film';
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  constructor() {}
 }
