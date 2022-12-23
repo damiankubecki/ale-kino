f326a436e7f114367ac6d4b0ef1c1119b4a799b0
Damian Kubecki <damian_094@wp.pl>
2022-12-23 10:50:22 +0100

    Add icons file

---
+++ src/assets/icons.ts
@@ -0,0 +1,9 @@
+import { faFacebook, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+
+export const icons = {
+  shoppingCart: faShoppingCart,
+  facebook: faFacebook,
+  instagram: faInstagram,
+  youtube: faYoutube,
+};
