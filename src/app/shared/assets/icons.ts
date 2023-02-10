43c43c0dfa7799f1c500dd2eb410822246fc96e5
Damian Kubecki <damian_094@wp.pl>
2023-02-10 09:49:45 +0100

    Update assets

--- src/app/shared/assets/icons.ts
+++ src/app/shared/assets/icons.ts
@@ -1,9 +1,10 @@
 import { faFacebook, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons';
-import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { faShoppingCart, faTrash } from '@fortawesome/free-solid-svg-icons';
 
 export const icons = {
   shoppingCart: faShoppingCart,
   facebook: faFacebook,
   instagram: faInstagram,
   youtube: faYoutube,
+  trash: faTrash,
 };
