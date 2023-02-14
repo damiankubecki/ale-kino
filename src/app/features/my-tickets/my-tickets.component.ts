b9e0f7acc6564568ca1eb116b801f576fe03e553
Damian Kubecki <damian_094@wp.pl>
2023-02-14 16:32:49 +0100

    Add my-tickets

---
+++ src/app/features/my-tickets/my-tickets.component.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { API_URL } from '@app/shared/data/api/api';
+import { TopbarService } from '@app/topbar.service';
+import { filter, map, Observable, of, switchMap } from 'rxjs';
+import { UserService } from '../auth/user/user.service';
+import { IOrder } from '../purchase/purchase.service';
+
+@Component({
+  selector: 'app-my-tickets',
+  templateUrl: './my-tickets.component.html',
+  styleUrls: ['./my-tickets.component.scss'],
+})
+export class MyTicketsComponent {
+  private http = inject(HttpClient);
+  private topbarService = inject(TopbarService);
+  private userService = inject(UserService);
+
+  myOrders$!: Observable<IOrder[]>;
+
+  constructor() {
+    this.topbarService.setTopbarContent('Moje bilety');
+
+    this.myOrders$ = this.userService.user$.pipe(
+      map(user => user.info?.ordersIds),
+      filter(Boolean),
+      switchMap(ids => {
+        const filters = ids?.map((id, index) => (index === 0 ? `?id=${id}` : `&id=${id}`)).join('');
+
+        return ids.length ? this.http.get<IOrder[]>(`${API_URL}/orders${filters}`) : of([]);
+      })
+    );
+  }
+}
