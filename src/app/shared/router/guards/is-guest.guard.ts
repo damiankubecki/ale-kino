9f7e490437fc6df961287703ca86fbfd13535486
Damian Kubecki <damian_094@wp.pl>
2023-02-20 13:53:05 +0100

    Update is-guest guard

--- src/app/shared/router/guards/is-guest.guard.ts
+++ src/app/shared/router/guards/is-guest.guard.ts
@@ -1,8 +1,8 @@
 import { inject, Injectable } from '@angular/core';
 import { Router, CanActivate } from '@angular/router';
-import { UserService } from '@app/features/auth/user/user.service';
+import { IUser, UserService } from '@app/features/auth/user/user.service';
 import { paths } from '@app/shared/router/paths';
-import { tap } from 'rxjs';
+import { debounceTime, filter, map, skip, switchMap, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -11,21 +11,17 @@ export class IsGuestGuard implements CanActivate {
   userService = inject(UserService);
   router = inject(Router);
 
-  canActivate(): boolean {
-    let isGuest = true;
+  canActivate() {
+    return this.checkUserRole();
+  }
 
-    this.userService.user$
-      .pipe(
-        tap(user => {
-          if (user.role === 'guest') {
-            isGuest = true;
-          } else {
-            this.router.navigate([paths.home]);
-          }
-        })
-      )
-      .subscribe();
-
-    return isGuest;
+  checkUserRole() {
+    return this.userService.getMe().pipe(
+      filter(Boolean),
+      tap(user => {
+        if (user.role !== 'guest') this.router.navigate([paths.home]);
+      }),
+      map(user => user.role === 'guest')
+    );
   }
 }
