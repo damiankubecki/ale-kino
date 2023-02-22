cdfe7104eacae55e39c024470aa1f853712c257b
Damian Kubecki <damian.kubecki@ppuc.pl>
2023-02-22 16:26:36 +0100

    Update admin module

--- src/app/features/admin/admin.module.ts
+++ src/app/features/admin/admin.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ADMIN_PATHS } from './admin-paths';
 import { AdminViewComponent } from './admin-view.component';
+import { MovieShowingsComponent } from './repertoire/movie-showings/movie-showings.component';
 import { RepertoireComponent } from './repertoire/repertoire.component';
 import { RoomsComponent } from './rooms/rooms.component';
 
@@ -12,6 +13,7 @@ import { RoomsComponent } from './rooms/rooms.component';
       { path: ADMIN_PATHS.default, component: AdminViewComponent },
       { path: ADMIN_PATHS.rooms, component: RoomsComponent },
       { path: ADMIN_PATHS.repertoire, component: RepertoireComponent },
+      { path: `${ADMIN_PATHS.repertoire}/:id`, component: MovieShowingsComponent },
     ]),
   ],
 })
