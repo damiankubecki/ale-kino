a99e02a060acfa3dbe8c3996e57df0f5f21463cf
Damian Kubecki <damian_094@wp.pl>
2023-02-23 14:31:07 +0100

    Update add-movie component

---
+++ src/app/features/admin/movies/add-movie-form/add-movie-form.component.ts
@@ -0,0 +1,106 @@
+import { AsyncPipe, NgFor, NgIf } from '@angular/common';
+import { ChangeDetectionStrategy, Component, EventEmitter, inject, Output } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  NonNullableFormBuilder,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatDialogModule, MatListModule } from '@angular/material';
+import { RouterModule } from '@angular/router';
+import { IMovie } from '@app/shared/types/interfaces';
+
+export type Form = FormGroup<{
+  title: FormControl<string>;
+  shortDescription: FormControl<string>;
+  longDescription: FormControl<string>;
+  imageURL: FormControl<string>;
+  duration: FormControl<string>;
+  minAge: FormControl<string>;
+  genre: FormControl<string>;
+  isPremiere: FormControl<boolean>;
+}>;
+
+@Component({
+  selector: 'app-add-movie-form',
+  standalone: true,
+  imports: [
+    RouterModule,
+    MatListModule,
+    MatDialogModule,
+    NgIf,
+    NgFor,
+    AsyncPipe,
+    ReactiveFormsModule,
+  ],
+  templateUrl: './add-movie-form.component.html',
+  styleUrls: ['./add-movie-form.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AddMovieFormComponent {
+  @Output() formValues = new EventEmitter();
+
+  private builder = inject(NonNullableFormBuilder);
+
+  form!: Form;
+  genres: string[] = [];
+
+  constructor() {
+    this.form = this.createForm();
+  }
+
+  handleSubmit() {
+    this.form.markAllAsTouched();
+
+    if (this.form.invalid) return;
+
+    const { title, shortDescription, longDescription, isPremiere, duration, minAge, imageURL } =
+      this.form.value;
+
+    const movieDTO: Partial<Omit<IMovie, 'id'>> = {
+      title,
+      description: {
+        short: shortDescription as string,
+        long: longDescription as string,
+      },
+      isPremiere,
+      duration,
+      minAge: Number(minAge) || null,
+      imageURL,
+      genres: this.genres,
+    };
+
+    this.formValues.emit(movieDTO);
+  }
+
+  addGenre() {
+    const genre = this.form.value.genre;
+
+    if (genre && !this.genres.includes(genre)) {
+      this.form.value.genre = '';
+      this.genres.push(genre);
+    }
+  }
+
+  removeGenre(genre: string) {
+    this.genres = this.genres.filter(genreItem => genreItem !== genre);
+  }
+
+  private createForm() {
+    const create = this.builder;
+
+    const form: Form = create.group({
+      title: create.control('', { validators: [Validators.required] }),
+      shortDescription: create.control('', { validators: [Validators.required] }),
+      longDescription: create.control('', { validators: [Validators.required] }),
+      imageURL: create.control(''),
+      duration: create.control('', { validators: [Validators.required] }),
+      minAge: create.control(''),
+      genre: create.control(''),
+      isPremiere: create.control(false),
+    });
+
+    return form;
+  }
+}
